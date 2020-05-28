6a162dbd4b5eeba8191a85da1dae2c953546562b
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-05-27 22:12:18 -0300

    main project file

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:contact_book/src/pages/homePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
